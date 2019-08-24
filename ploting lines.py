c59a1a857e5527bfe9a62fcb9bdcd76812dc1c70
Adlin Jospin Joeph <53537050+adlinjoseph96@users.noreply.github.com>
2019-08-24 10:05:43 +0530

    simple program to draw graph using python

---
+++ ploting lines.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+x1=[10,1,4,8]
+y1=[2,7,4,6]
+plt.plot(x1,y1,label='line1')
+x2=[3,1,12,8]
+y2=[9,6,4,3]
+plt.plot(x2,y2,label='line2')
+plt.xlabel ('x axis')
+plt.ylabel ('y axis')
+plt.show()
+
+
